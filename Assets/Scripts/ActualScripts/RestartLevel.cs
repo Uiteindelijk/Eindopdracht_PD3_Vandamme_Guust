5b21aba6902bac0bde0a00abe56377198a4884ff
DESKTOP-4TR8F29\Guust <diedriek@gmail.com>
2019-01-17 12:57:28 +0100

    update 2.1
    Level aanpassingen etc

---
+++ Assets/Scripts/ActualScripts/RestartLevel.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class RestartLevel : MonoBehaviour
+{
+
+    private void Update()
+    {
+        if(Input.GetButtonDown("RestartButton"))
+        {
+            Restart();
+        }
+    }
+
+    private void Restart()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name); //om ons level te herstarten
+
+        Time.timeScale = 1f;                        //voor als het spel gepauzeerd was dat de tijd teru doorloopt
+
+        Cursor.lockState = CursorLockMode.Locked;   //om onze muis vast te zetten
+        Cursor.visible = false;                     //om onze muis onzichtbaar te maken
+
+    }
+}
